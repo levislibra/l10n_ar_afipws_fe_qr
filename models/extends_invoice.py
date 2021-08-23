cd4532e875a022601f1572578bb9c245b6826d62
Levis <levislibra@hotmail.com>
2021-08-23 11:44:06 -0300

    Update

--- models/extends_invoice.py
+++ models/extends_invoice.py
@@ -6,6 +6,7 @@ import base64
 import qrcode
 from io import BytesIO
 from PIL import Image
+from datetime import datetime
 
 class ExtendsAccountInvoice(models.Model):
 	_inherit = "account.invoice"
@@ -17,10 +18,10 @@ class ExtendsAccountInvoice(models.Model):
 	@api.one
 	def _compute_json_qr(self):
 		print("_compute_json_qr")
-		if self.type in ['out_invoice','out_refund'] and self.state in ['open','paid'] and self.afip_auth_code != False:
+		if (self.type in ['out_invoice','out_refund'] and self.state in ['open','paid'] and self.afip_auth_code != False):
 			dict_invoice = {
 				"ver": 1,
-				"fecha": str(self.date_invoice),
+				"fecha": self.date_invoice,
 				"cuit": int(self.company_id.main_id_number),
 				"ptoVta": self.journal_id.point_of_sale_number,
 				"tipoCmp": int(self.journal_document_type_id.document_type_id.code),
@@ -33,19 +34,17 @@ class ExtendsAccountInvoice(models.Model):
 				"tipoCodAut": 'E',
 				"codAut": int(self.afip_auth_code),
 			}
+			# res = ','.join(['%s:%s' % (key,value) for (key, value) in dict_invoice.items()])
 			res = str(dict_invoice).replace("\n", "")
 			res = res.replace(" ", "")
-			print("RES:: ", res)
 		else:
 			dict_invoice = 'ERROR'
 			res = 'N/A'
 		self.json_qr = res
 		if type(dict_invoice) == dict:
-			print("res 2: ",res)
-			enc = res.encode()
-			b64 = base64.encodestring(enc)
-			print("b64:: ", b64)
-			b64 = b64.replace('\n', '')
+			b64 = res.encode('base64','strict')
+			# enc = res.encode()
+			# b64 = base64.encodestring(enc)
 			self.texto_modificado_qr = 'https://www.afip.gob.ar/fe/qr/?p=' + str(b64)
 		else:
 			self.texto_modificado_qr = 'https://www.afip.gob.ar/fe/qr/?ERROR'
@@ -53,23 +52,38 @@ class ExtendsAccountInvoice(models.Model):
 
 	@api.one
 	def _compute_qr(self):
-		# Link for website
-		# input_data = "https://towardsdatascience.com/face-detection-in-10-lines-for-beginners-1787aa1d9127"
 		#Creating an instance of qrcode
-		qr = qrcode.QRCode(
-			version=1,
-			box_size=2,
-			border=0)
-		qr.add_data(self.texto_modificado_qr)
-		qr.make(self.texto_modificado_qr)
-		img = qr.make_image()
-
+		# qr = qrcode.QRCode(
+		# 	version=1,
+		# 	box_size=2,
+		# 	border=1)
+		# qr.add_data(self.texto_modificado_qr)
+		# qr.make(self.texto_modificado_qr, image_factory=PymagingImage)
+		# img = qr.make_image()
+		# print("img:: ", img)
 		# img = qrcode.make(self.texto_modificado_qr)
 
-		maxsize = (120, 120)
-		img.thumbnail(maxsize, Image.ANTIALIAS)
+		# maxsize = (115, 115)
+		# img.thumbnail(maxsize, Image.ANTIALIAS)
+
+		# buffered = BytesIO()
+		# img.save(buffered, format="JPEG")
+		# img_str = base64.b64encode(buffered.getvalue())
+		qr = qrcode.QRCode(
+			version=1,
+			error_correction=qrcode.constants.ERROR_CORRECT_H,
+			box_size=4,
+			border=4,
+		)
+
+		qr.add_data(self.texto_modificado_qr)
+		qr.make(fit=True)
+		img = qr.make_image()
+		print("img:: ", img)
+		img = img.resize((115, 115), Image.NEAREST)
 
 		buffered = BytesIO()
-		img.save(buffered, format="JPEG")
-		img_str = base64.b64encode(buffered.getvalue())
+		img.save(buffered, format="PNG")
+		print("img:: ", img)
+		img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
 		self.qr = img_str
