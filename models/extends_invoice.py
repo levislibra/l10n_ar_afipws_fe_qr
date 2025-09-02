559aa678c03f59d9a0909d4531e42a237582e2fd
Levis <levislibra@hotmail.com>
2025-09-02 00:26:57 +0000

    Update

--- models/extends_invoice.py
+++ models/extends_invoice.py
@@ -30,6 +30,7 @@ class ExtendsAccountInvoice(models.Model):
 	
 	@api.one
 	def update_date_move(self):
+		self.date = self.date_invoice
 		self.move_id.date = self.date_invoice
 		self.move_id.date_due = self.date_invoice
 		# for move_line_id in self.move_id.line_ids:
